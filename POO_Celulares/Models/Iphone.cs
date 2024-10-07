52818073ce529e8e6e2ec561e962f6edb053075b
Marco.A.D.S <marcosofth@gmail.com>
2024-10-07 19:28:04 -0300

    treinando POO

---
+++ POO_Celulares/Models/Iphone.cs
@@ -0,0 +1,19 @@
+using POO_Celulares.Models;
+
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
+    {
+        public Iphone (string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando o aplicativo " + nomeApp);
+        }
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+    }
+}
---
+++ POO_Celulares/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+using POO_Celulares.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("98899117", "Iphone15", "06413317116", 32);
+iphone.InstalarAplicativo("Instagram");
+
+
+
+Nokia nokia = new Nokia("33487209", "Tijolão", "11122233344", 16);
+nokia.InstalarAplicativo("Facebook");
+
+
+
